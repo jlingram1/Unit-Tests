523cfa63d90a62de193b13b18f0c541eed51221e
jlingram1 <janaingram@Janas-MacBook-Air.local>
2020-03-06 14:47:16 -0600

    modified file path

--- GitHub_User_Search.py
+++ GitHub_User_Search.py
@@ -10,17 +10,21 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
+# Set up unit test
 
 class Google_Test(unittest.TestCase):
     
     def set_up(self):
-        self.driver = webdriver.Chrome(executable_path='/users/janaingram/downloads/chromedriver')
+        self.driver = webdriver.Chrome(executable_path='/file/path/for/chromedriver')
         self.driver.implicitly_wait(10)
         self.driver.maximize_window()
         
     def test_search(self):
-        self.driver = webdriver.Chrome(executable_path='/users/janaingram/downloads/chromedriver')
+        self.driver = webdriver.Chrome(executable_path='/file/path/for/chromedriver')
         self.driver.get("https://www.github.com/")
+        
+        # Searching for user: jlingram1
+        
         self.driver.find_element_by_name('q').send_keys("jlingram1")
         self.driver.find_element_by_name("q").send_keys(Keys.RETURN)
       
